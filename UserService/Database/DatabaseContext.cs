1e4e019cbf0fec9f30f351f777a8fb11e60c0ba7
mohamad.hariri <>
2021-12-21 23:23:55 +0200

    CMS Authentication , JWT Creation + User Roles Using Microsoft Identity

---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using AdService.Authentication;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdService.Database
+{
+    public class DatabaseContext  : IdentityDbContext<ApplicationUser>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
