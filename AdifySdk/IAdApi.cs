a45a1f50cfdba832ef546d2b6143511d53c19519
mohamad.hariri <>
2021-12-26 18:48:49 +0200

    Ad Api Sdk Implementation

---
+++ AdifySdk/IAdApi.cs
@@ -0,0 +1,19 @@
+﻿using AdifyContracts.Ads.Response;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdifySdk
+{
+    public interface IAdApi
+    {
+        [Get("/api/Ads/sdk/getAd")]
+        Task<ApiResponse<AdResponse>> GetRandomAdAsync();
+
+        [Get("/api/Ads/getAdsById/{Id}")]
+        Task<ApiResponse<GetAdByCampainResponse>> GetAdByCampainAsync(int Id);
+
+    }
+}
