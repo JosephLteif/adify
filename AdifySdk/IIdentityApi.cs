4c5de523904d2247b53648deae010e57096ede54
mohamad.hariri <>
2021-12-26 18:48:23 +0200

    Identity Api Sdk Implementation

---
+++ AdifySdk/IIdentityApi.cs
@@ -0,0 +1,21 @@
+﻿
+using AdifyContracts.Identity;
+using AdifyContracts.Response;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdifySdk
+{
+    public interface IIdentityApi
+    {
+        [Post("/api/authenticate/register")]
+        Task<ApiResponse<AuthSuccessResponse>> RegisterAsync([Body]RegisterRequest registerRequest);
+        [Post("/api/authenticate/register-admin")]
+        Task<ApiResponse<AuthSuccessResponse>> RegisterAdminAsync([Body]RegisterRequest registerRequest);
+        [Post("/api/authenticate/login")]
+        Task<ApiResponse<AuthSuccessResponse>> LoginAsync([Body]LoginRequest registerRequest);
+    }
+}
